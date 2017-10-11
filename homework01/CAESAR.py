398e1645b70ddf9232a13cd00bc0d2cc6f6fac18
Ylikn <31666147+Ylikn@users.noreply.github.com>
2017-10-11 16:33:45 +0300

    Uploaded for check

---
+++ homework01/CAESAR.py
@@ -0,0 +1,57 @@
+def encrypt_caesar(plaintext, shift):
+    """
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("")
+    ''
+    """
+    text = str(plaintext)
+    ciphertext = ""
+    alp = 26
+    sp = shift % 26
+    for i in range(len(text)):
+        char = text[i]
+        if char in '1234567890':
+            ciphertext += char
+            continue
+        if not ((97 <= ord(char) <= 122) or (65 <= ord(char) <= 90)):
+            ciphertext += char
+            continue
+        if (97 <= ord(char) + sp <= 122) or (65 <= ord(char) + sp <= 90):
+            ciphertext += chr(ord(char) + sp)
+        else:
+            ciphertext += chr(ord(char) + alp + sp)
+    return ciphertext
+
+
+def decrypt_caesar(ciphertext, shift):
+    """
+    >>> decrypt_caesar("SBWKRQ")
+    'PYTHON'
+    >>> decrypt_caesar("sbwkrq")
+    'python'
+    >>> decrypt_caesar("")
+    ''
+    """
+    text = str(ciphertext)
+    plaintext = ""
+    alp = 26
+    sp = shift % 26
+    for i in range(len(text)):
+        char = text[i]
+        if char in '1234567890':
+            plaintext += char
+            continue
+        if not ((97 <= ord(char) <= 122) or (65 <= ord(char) <= 90)):
+            plaintext += char
+            continue
+        if (97 <= ord(char)-sp <= 122) or (65 <= ord(char)-sp <= 90):
+            plaintext += chr(ord(char)-sp)
+        else:
+            plaintext += chr(ord(char)+alp-sp)
+    return plaintext
+
+
+print(decrypt_caesar("jrbhglkwejbgukbelrg , , , , 11 12 4 35", 70))
